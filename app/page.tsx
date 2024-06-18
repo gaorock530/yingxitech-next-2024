fc6efd76a18674ae6cbc5a53e1bdc9f1c109cf18
gaorock530 <gaorock530@hotmail.com>
2024-06-18 16:40:55 +0800

    add pay

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-
-import Image from 'next/image'
-import styles from './Home.module.css';
+import Image from "next/image";
+import styles from "./Home.module.css";
 // Import Swiper React components
 // import { Swiper, SwiperSlide } from 'swiper/react';
 // import Swiper core and required modules
 // import { Keyboard, Pagination, Mousewheel } from 'swiper/modules';
-import Form from './form';
+import Form from "./form";
 // Import Swiper styles
 // import 'swiper/css';
 // import 'swiper/css/bundle'
 
 export default function Home() {
-
   return (
     <main className={styles.main}>
       <header>
@@ -20,12 +18,8 @@ export default function Home() {
       </header>
       <div className={styles.pageWrapper}>
         <div className={styles.pageContent}>
-          <p className="page-1-p">
-            科技改变生活，技术推动思维
-          </p>
-          <p className="page-1-p">
-            打造互联网运营新模式
-          </p>
+          <p className="page-1-p">科技改变生活，技术推动思维</p>
+          <p className="page-1-p">打造互联网运营新模式</p>
         </div>
         <div className={styles.pageMask}></div>
         <img className={styles.bgImg} src="/img/1.jpg" />
@@ -48,86 +42,27 @@ export default function Home() {
       </div>
       <div className={styles.pageWrapper}>
         <div className={styles.pageContent}>
+          <div className="consult">
+            <p>
+              承接各种网站应用、手机APP、微信支付宝小程序开发业务，和各种技术类合作，包括AI算法、大数据分析、区块链、加密算法等。
+            </p>
+            <button>付费咨询</button>
+          </div>
           <footer>
-            <p><span>影袭网络技术有限公司 © 2024</span><span>All rights reserved.</span></p>
-            <p><a href="https://beian.miit.gov.cn/#/Integrated/index">豫ICP备18021827号</a></p>
+            <p>
+              <span>影袭网络技术有限公司 © 2024</span>
+              <span>All rights reserved.</span>
+            </p>
+            <p>
+              <a href="https://beian.miit.gov.cn/#/Integrated/index">
+                豫ICP备18021827号
+              </a>
+            </p>
           </footer>
         </div>
         <div className={styles.pageMask}></div>
         <img className={styles.bgImg} src="/img/4.jpg" />
       </div>
-      {/* <Swiper
-        // modules={[Pagination, Keyboard, Mousewheel]}
-        // spaceBetween={0}
-        // direction={'vertical'}
-        // // slidesPerView={1}
-        // mousewheel
-        // keyboard={{ enabled: true }}
-        // pagination={{
-        //   "clickable": true
-        // }}
-        // // controller={{ control: controlledSwiper }}
-        // onSlideChange={(e) => console.log('slide change', e.snapIndex)}
-        // onSwiper={(swiper) => console.log(swiper)}
-        >
-          <SwiperSlide>
-            <div className={styles.pageWrapper}>
-              <div className={styles.pageContent}>
-                <p className="page-1-p">
-                  科技改变生活，技术推动思维
-                </p>
-                <p className="page-1-p">
-                  打造互联网运营新模式
-                </p>
-              </div>
-              <div className={styles.pageMask}></div>
-              <img className={styles.bgImg} src="/img/1.jpg" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.pageWrapper}>
-              <div className={styles.pageContent}>
-                <p className="page-1-p">
-                  常年在北京、上海、香港、台湾、新加坡、马来西亚、印尼、泰国、韩国、日本、英国等国家和地区联合举办各种大赛在河南地区的推广宣传
-                </p>
-              </div>
-              <div className={styles.pageMask}></div>
-              <img className={styles.bgImg} src="/img/2.jpg" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.pageWrapper}>
-              <div className={styles.pageContent}>
-                <form onSubmit={onSubmit}>
-                  <h3>联系我们</h3>
-                  <h5>个人/单位:</h5>
-                  <input type="text" placeholder="姓名/名称" autoComplete="off" name="name" />
-                  <h5>联系电话:</h5>
-                  <input type="phone" placeholder="手机/座机" autoComplete="off" name="phone" pattern="[0-9]+" />
-                  <h5>留言:</h5>
-                  <textarea name="msg"></textarea>
-                  <button>提交</button>
-                </form>
-              </div>
-              <div className={styles.pageMask}></div>
-              <img className={styles.bgImg} src="/img/3.jpg" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.pageWrapper}>
-              <div className={styles.pageContent}>
-                <footer>
-                  <p><span>影袭网络技术有限公司 © 2024</span><span>All rights reserved.</span></p>
-                  <p><a href="https://beian.miit.gov.cn/#/Integrated/index">豫ICP备18021827号</a></p>
-                </footer>
-              </div>
-              <div className={styles.pageMask}></div>
-              <img className={styles.bgImg} src="/img/4.jpg" />
-            </div>
-          </SwiperSlide>
-
-        </Swiper> */}
-
     </main>
-  )
+  );
 }

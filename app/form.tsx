61979f5c6fc9f479a22a94d52e36b8c372a34665
gaorock530 <gaorock530@hotmail.com>
2024-06-27 01:22:41 +0800

    form

--- app/form.tsx
+++ app/form.tsx
@@ -1,15 +1,26 @@
 'use client'
 
+import { useRef, useState } from "react";
+import Snackbar  from '@mui/material/Snackbar';
+
 export default function Form() {
+  const [open ,setOpen] = useState(false)
+
   const onSubmit = (e: any) => {
     e.preventDefault();
-    // console.log('submit');
+    e.currentTarget.reset()
+    setOpen(true)
     // const data = new FormData(e.target);
     // console.log(data.get('name'))
     // console.log(data.get('phone'))
     // console.log(data.get('msg'))
+
   }
-  return <form onSubmit={onSubmit}>
+
+
+  return (
+    <>
+  <form onSubmit={onSubmit} style={{position: "relative"}}>
     <h3>联系我们</h3>
     <h5>个人/单位:</h5>
     <input type="text" placeholder="姓名/名称" autoComplete="off" name="name" />
@@ -18,5 +29,13 @@ export default function Form() {
     <h5>留言:</h5>
     <textarea name="msg"></textarea>
     <button>提交</button>
+    
   </form>
+    <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        open={open}
+        onClose={() => setOpen(false)}
+        message="联系信息已提交"
+      />
+    </>)
 }

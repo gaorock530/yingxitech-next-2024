86b9f9889e299e646d7ef997a18fc49284ea4b66
gaorock530 <gaorock530@hotmail.com>
2024-07-26 18:13:36 +0800

    update

--- app/check/page.tsx
+++ app/check/page.tsx
@@ -2,8 +2,29 @@ import * as React from "react";
 import style from "./Page.module.css";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useSearchParams } from "next/navigation";
+import serverRequest from "./request";
+
+const types = ["warn", "notice", "order", "register"];
 
 export default function CheckPage() {
+  const searchParams = useSearchParams();
+  const type = searchParams.get("type");
+
+  React.useEffect(() => {
+    if (!type || !types.includes(type)) return;
+    (async function () {
+      try {
+        const res = await serverRequest(`/auth/getOneNoticePage?type=${type}`);
+        console.log(res);
+      } catch (e) {
+        console.log(e);
+      } finally {
+      }
+    })();
+  }, []);
+
   return (
     <div className={style.container}>
       <Grid container spacing={2}>

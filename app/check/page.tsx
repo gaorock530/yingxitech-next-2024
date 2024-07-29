0a7674c373aee195c95eb1eb510988c9f98e90c8
gaorock530 <gaorock530@hotmail.com>
2024-07-29 18:21:42 +0800

    fix

--- app/check/page.tsx
+++ app/check/page.tsx
@@ -1,12 +1,10 @@
-"use client"
+"use client";
 import * as React from "react";
 import style from "./Page.module.css";
 import Paper from "@mui/material/Paper";
 import CircularProgress from "@mui/material/CircularProgress";
 import { useSearchParams } from "next/navigation";
 import serverRequest from "./request";
-
-
 
 const types = ["warn", "notice", "order", "register"];
 
@@ -15,32 +13,51 @@ export default function CheckPage() {
   const type = searchParams.get("type");
   const value = searchParams.get("value");
 
-  const [loading, setLoading] = React.useState(false)
-  const [data, setData] = React.useState<Record<string, any>>({})
+  const [loading, setLoading] = React.useState(false);
+  const [data, setData] = React.useState<Record<string, any>>({});
 
   React.useEffect(() => {
     if (!value || !type || !types.includes(type)) return;
     (async function () {
       try {
-        setLoading(true)
-        const res = await serverRequest(`/auth/getOneNoticePage?type=${type}&value=${value}`, {action : {action: 'type', value: 'value'}});
+        setLoading(true);
+        const res = await serverRequest(
+          `/auth/getOneNoticePage?type=${type}&value=${value}`,
+          { action: { action: "type", value: "value" } }
+        );
         console.log(res);
-        if (!res) return
-        setData(res)
+        if (!res) return;
+        setData(res);
       } catch (e) {
         console.log(e);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
     })();
   }, [type, value]);
 
-  const RenderData = () => Object.keys(data).length > 0 ? Object.keys(data).map((key: string) => <p key={key}><b>{key}:</b> <span className={style.detail}>{data[key]}</span></p>): <p>NO DATA</p>
+  const RenderData = () =>
+    Object.keys(data).length > 0 ? (
+      Object.keys(data).map((key: string) => (
+        <p key={key}>
+          <b>{key}:</b>{" "}
+          <span className={style.detail}>
+            {typeof data[key] === "object"
+              ? JSON.stringify(data[key])
+              : data[key]}
+          </span>
+        </p>
+      ))
+    ) : (
+      <p>NO DATA</p>
+    );
 
   return (
     <div className={style.container}>
-      <Paper className={style.item}>{type || 'NO TYPE'}</Paper>
-      <Paper className={style.item}>{loading? <CircularProgress />:<RenderData />}</Paper>
+      <Paper className={style.item}>{type || "NO TYPE"}</Paper>
+      <Paper className={style.item}>
+        {loading ? <CircularProgress /> : <RenderData />}
+      </Paper>
     </div>
   );
 }

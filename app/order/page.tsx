aab62e42a0630f229d1da692b52decb0c11c0ccd
gaorock530 <gaorock530@hotmail.com>
2025-02-12 23:35:34 +0800

    order

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -4,42 +4,36 @@ import style from "./Page.module.css";
 import Paper from "@mui/material/Paper";
 import CircularProgress from "@mui/material/CircularProgress";
 import { useSearchParams } from "next/navigation";
-import serverRequest from "./request";
+// import serverRequest from "./request";
 
 const types = ["paid", "sent", "received", "done", "other"];
 
 export default function OrderPage() {
   const searchParams = useSearchParams();
   const status = searchParams.get("status");
-  const orderId = searchParams.get("orderId");
+  const orderNo = searchParams.get("orderNo");
+  const openid = searchParams.get("openid");
 
   const [loading, setLoading] = React.useState(false);
   const [data, setData] = React.useState<Record<string, any>>({});
   const [result, setResult] = React.useState<any>("no result");
 
   React.useEffect(() => {
-    if (!status || !orderId || !types.includes(status)) {
+    if (!status || !orderNo || !types.includes(status) || !openid) {
       return;
     }
     (async function () {
       try {
         setLoading(true);
-        const res = await fetch(
-          `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe82604d9a68ca8cf&redirect_uri=https%3A%2F%2Fapi.yingxitech.com%2Fauth%2FgetOpenidFromCodeRedirect&response_type=code&scope=snsapi_base#wechat_redirect`,
-          {
-            mode: "no-cors",
-          }
-        );
-        const json = await res.json();
-        console.log({ res });
-        setResult(json);
-        // const res = await serverRequest(
-        //   `/auth/getOneNoticePage?type=${type}&value=${value}`,
-        //   { action: { action: "type", value: "value" } }
+        // const res = await fetch(
+        //   `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxe82604d9a68ca8cf&redirect_uri=https%3A%2F%2Fapi.yingxitech.com%2Fauth%2FgetOpenidFromCodeRedirect&response_type=code&scope=snsapi_base#wechat_redirect`,
+        //   {
+        //     mode: "no-cors",
+        //   }
         // );
-        // console.log(res);
-        // if (!res) return;
-        // setData(res);
+        // const json = await res.json();
+        // console.log({ res });
+        // setResult(json);
       } catch (e: any) {
         console.log(e);
         setResult(e.toString());
@@ -47,7 +41,7 @@ export default function OrderPage() {
         setLoading(false);
       }
     })();
-  }, [status, orderId]);
+  }, [status, orderNo, openid]);
 
   const RenderData = () =>
     Object.keys(data).length > 0 ? (
@@ -73,7 +67,8 @@ export default function OrderPage() {
             ? `status: ${status.toUpperCase()}`
             : "NO Status"}
         </p>
-        <p>{orderId ? `orderId: ${orderId}` : "NO orderId"}</p>
+        <p>{orderNo ? `orderNo: ${orderNo}` : "NO orderId"}</p>
+        <p>{openid ? `openid: ${openid}` : "NO openid"}</p>
       </Paper>
       <Paper className={style.item}>
         {loading ? <CircularProgress /> : <RenderData />}

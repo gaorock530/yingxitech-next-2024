5db8ced1cbc4a939cb913524fcd92cb79a82ec19
gaorock530 <gaorock530@hotmail.com>
2025-02-24 10:21:08 +0800

    fix orders page

---
+++ app/orders/layout.tsx
@@ -0,0 +1,10 @@
+import { Suspense } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+
+export default function CheckLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <Suspense fallback={<CircularProgress />}>{children}</Suspense>;
+}
